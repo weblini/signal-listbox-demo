04b0821cb78471e216bfa1e900635e2bf7139d12
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-05-10 12:35:44 +0200

    Added selection state to URL query parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,56 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { Listbox, Option, Orientation } from './listbox';
 import { Vegetable, VegetablesService } from './vegetables.service';
 import { CardComponent } from './card/card.component';
-import { JsonPipe } from '@angular/common'
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, Listbox, Option, CardComponent, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'try-signals';
 
   private vegetableService = inject(VegetablesService);
-  protected readonly availableVegetables = this.vegetableService.getVegetables();
-  selectedToppings: Vegetable[] = [];
+  protected readonly availableVegetables =
+    this.vegetableService.getVegetables();
+  protected selectedToppings: Vegetable[] = [];
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   protected orientation = signal<Orientation>('vertical');
 
-  logValueChange(newValue: Vegetable[]) {
-    console.log(newValue);
+  constructor() {
+    this.route.queryParamMap.subscribe((queryParams) => {
+      const currentIds = queryParams.getAll('ids');
+
+      let selectionFromQuery: Vegetable[] = [];
+      for (const id of currentIds) {
+        const matchedVegetable = this.availableVegetables.find(
+          (vegetable) => vegetable.id === Number(id)
+        );
+        if (matchedVegetable) {
+          selectionFromQuery.push(matchedVegetable);
+        }
+      }
+
+      this.selectedToppings = selectionFromQuery;
+    });
+  }
+
+  updateQueryParams(newSelection: Vegetable[]) {
+    const newIds = newSelection.map((vegetable) => vegetable.id);
+
+    this.router.navigate([], {
+      queryParams: {
+        ids: newIds,
+      },
+    });
   }
 
   logOrientationChange(newOrientation: Orientation) {
@@ -30,9 +58,8 @@ export class AppComponent {
   }
 
   toggleOrientation() {
-    this.orientation.update(oldValue =>
-      oldValue === 'horizontal' ?
-        'vertical' :
-        'horizontal');
+    this.orientation.update((oldValue) =>
+      oldValue === 'horizontal' ? 'vertical' : 'horizontal'
+    );
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, input } from '@angular/core';
 
+// TODO: animate cards enter/leave view
+
 @Component({
   selector: 'app-card',
   standalone: true,
