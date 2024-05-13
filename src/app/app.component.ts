030f8fea8e536efa36439e68cce927ebe9695cf5
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-05-13 13:14:00 +0200

    Added @empty control flow block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,9 @@ import {
       transition(':enter', [style({opacity: 0}), animate('250ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({opacity: 1}))]),
       transition(':leave', [animate('200ms cubic-bezier(0.4, 0.0, 1, 1)', style({opacity: 0}))]),
     ]),
+    trigger('offsetEnter', [
+      transition(':enter', [style({opacity: 0}), animate('250ms 250ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({opacity: 1}))]),
+    ]),
   ],
 })
 export class AppComponent {
