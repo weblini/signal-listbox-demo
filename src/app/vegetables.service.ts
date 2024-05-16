700a8859d38f3ee8f8878cdefc90b7ebff160a68
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-05-16 08:07:24 +0200

    Extended Vegetable Service functionality & updated styles

--- src/app/vegetables.service.ts
+++ src/app/vegetables.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
 export interface Vegetable {
@@ -12,10 +12,33 @@ export interface Vegetable {
 })
 export class VegetablesService {
   readonly #VEGETABLE_URL = 'http://localhost:3000/vegetables';
+  readonly #JSON_HEADERS = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
   http = inject(HttpClient);
 
+  // TODO: handle errors
+
   getVegetables() {
     return this.http.get<Vegetable[]>(this.#VEGETABLE_URL);
+  }
+
+  deleteVegetable(id: number) {
+    return this.http.delete<Vegetable>(this.#VEGETABLE_URL + '/' + id);
+  }
+
+  addVegetable(newVegetable: Vegetable) {
+    return this.http.post<Vegetable>(this.#VEGETABLE_URL, newVegetable, {
+      headers: this.#JSON_HEADERS,
+      observe: 'response',
+    });
+  }
+
+  updateVegetable(newVegetable: Vegetable) {
+    return this.http.put<Vegetable>(this.#VEGETABLE_URL, newVegetable, {
+      headers: this.#JSON_HEADERS,
+      observe: 'response',
+    });
   }
 }
