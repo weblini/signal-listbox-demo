707c5c1c698fa4657a7e233f2e9f8ee76eb3ab89
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-06-03 10:28:15 +0200

    Reintroduced success & error messages

--- src/app/presentation/services/event-notification.service.ts
+++ src/app/presentation/services/event-notification.service.ts
@@ -1,18 +1,44 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { filter, map, merge, tap } from 'rxjs';
 import { FreeToast, Toast } from '@ui/models';
-
-
+import { AuthStore } from '@core/store/auth.store';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { Status } from '@core/models';
+import { VegetableStore } from '@core/store/vegetables.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventNotificationService {
-  readonly eventStream$ = new Subject<Toast>();
+  readonly #authStore = inject(AuthStore);
+  readonly #vegetableStore = inject(VegetableStore);
+
+  protected readonly authEvents$ = toObservable(this.#authStore.status).pipe(
+    filter((status) => status === Status.Error || status === Status.Success),
+    map(
+      (status): FreeToast => ({
+        message: this.#authStore.message(),
+        type: status === Status.Error ? 'error' : 'success',
+      }),
+    ),
+  );
+
+  protected readonly vegetableEvents$ = toObservable(
+    this.#vegetableStore.saveStatus,
+  ).pipe(
+    filter((status) => status === Status.Error || status === Status.Success),
+    map(
+      (status): FreeToast => ({
+        message: this.#vegetableStore.message(),
+        type: status === Status.Error ? 'error' : 'success',
+      }),
+    ),
+  );
+
   #idCount = 1;
 
-  toastEvent(toast: FreeToast) {
-    this.eventStream$.next({ ...toast, id: this.#idCount });
-    this.#idCount++;
-  }
+  readonly eventStream$ = merge(this.authEvents$, this.vegetableEvents$).pipe(
+    map((toast): Toast => ({ ...toast, id: this.#idCount })),
+    tap(() => this.#idCount++),
+  );
 }
