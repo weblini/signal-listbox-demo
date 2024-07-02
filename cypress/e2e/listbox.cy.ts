4abf1503ef4cb4cf600fbd41ba0c05ee9afc86d3
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-07-02 15:05:14 +0200

    Moved listbox tests

--- cypress/e2e/listbox.cy.ts
+++ cypress/e2e/listbox.cy.ts
@@ -1,14 +1,7 @@
-// import { getGreeting } from '../support/app.po';
-
-describe('main view', () => {
+describe('veggie listbox', () => {
   beforeEach(() => cy.visit('http://localhost:4200/'));
 
   it('displays a details card of the selected item', () => {
-    // Custom command example, see `../support/commands.ts` file
-    // cy.login('my-email@something.com', 'myPassword');
-    // Function helper example, see `../support/app.po.ts` file
-    // getGreeting().contains(/Welcome/);
-
     cy.get('[data-test=option-name]:first')
       .should('exist')
       .click()
@@ -18,5 +11,9 @@ describe('main view', () => {
           .should('exist')
           .contains('.title', veggieName);
       });
+  });
+
+  it('indicates when it is selected', () => {
+    cy.get('jm-option:first').click().contains('✓');
   });
 });
