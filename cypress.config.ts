42980795a208442a8d4685403a90f43a9fe4afe8
Alina Łaszcz <alina.laszcz@studio2wx.com>
2024-07-02 08:46:53 +0200

    Added Cypress E2E

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'cypress',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run try-signals:serve',
+        production: 'nx run try-signals:serve:production',
+      },
+    }),
+  },
+});
